57e6be61e4c7d145828abafb9cd2f65d77db80df
vijaykanth1729 <personalvijay007@gmail.com>
2019-09-11 10:34:00 +0530

    updated color

--- account/views.py
+++ account/views.py
@@ -13,8 +13,8 @@ def registration(request):
             form.save()
             # email = form.cleaned_data.get('email')
             # raw_password = form.cleaned_data.get('password1')
-            # account = authenticate(email=email, password=raw_password)
-            # login(request, account)
+            # user = authenticate(email=email, password=raw_password)
+            #login(request, account)
             return render(request,'account/success_registration.html')
         else:
             context['registration_form'] = form

06f34dc41c4dc24e498384fdc6e6b9a847d815ca
vijaykanth1729 <personalvijay007@gmail.com>
2019-09-23 10:27:49 +0530

    this is an updated time version

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class Student(models.Model):
     first_name = models.CharField(max_length=100)
@@ -16,6 +16,8 @@ class Contact(models.Model):
     Email = models.EmailField()
     Subject = models.CharField(max_length=200)
     Message = models.TextField()
+    created_on = models.DateTimeField(default=timezone.now, auto_now=False)
+
 
     def __str__(self):
         return self.Email
--- website/settings.py
+++ website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8ik=vhk5+c4o5a9pbkg&83eh%efq1ty5uh%!$qy4&kew%5vqu('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
